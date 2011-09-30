158ca26fd1b3444d5da820a18821886d06c5cca7
mark@g.foster.cc <mark@g.foster.cc@c916d4ac-883d-2cb1-7cf7-9943a70a09b0>
2011-09-30 17:28:32 +0000

    Patch from jonathanmeeks@gmail.com to format output correctly

--- trunk/ExportPriv.java
+++ trunk/ExportPriv.java
@@ -9,64 +9,88 @@ import java.security.cert.Certificate;
 import java.security.*;
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.Vector;
 
 class ExportPriv {
-    public static void main(String args[]) throws Exception{
-		if (args.length < 2) {
-			//Yes I know this sucks (the password is visible to other users via ps
-			// but this was a quick-n-dirty fix to export from a keystore to pkcs12
-			// someday I may fix, but for now it'll have to do.
-			System.err.println("Usage: java ExportPriv <keystore> <alias> <password>");
-			System.exit(1);
-		}
-		ExportPriv myep = new ExportPriv();
-		myep.doit(args[0], args[1], args[2]);
+  public static void main(String args[]) throws Exception{
+    if (args.length < 2) {
+      //Yes I know this sucks (the password is visible to other users via ps
+      // but this was a quick-n-dirty fix to export from a keystore to pkcs12
+      // someday I may fix, but for now it'll have to do.
+      System.err.println("Usage: java ExportPriv <keystore> <alias> <password>");
+      System.exit(1);
     }
+    ExportPriv myep = new ExportPriv();
 
-    public void doit(String fileName, String aliasName, String pass) throws Exception{
+    // System.out.println("Args: " + args[0] + " " + args[1] + " " + args[2]);
+                
 
-	KeyStore ks = KeyStore.getInstance("JKS");
+    myep.doit(args[0], args[1], args[2]);
+  }
 
-	char[] passPhrase = pass.toCharArray();
-	//BASE64Encoder myB64 = new BASE64Encoder();
+  public void doit(String fileName, String aliasName, String pass) throws Exception{
 
-	File certificateFile = new File(fileName);
-	ks.load(new FileInputStream(certificateFile), passPhrase);
+    KeyStore ks = KeyStore.getInstance("JKS");
 
-	KeyPair kp = getPrivateKey(ks, aliasName, passPhrase);
-		
-	PrivateKey privKey = kp.getPrivate();
+    char[] passPhrase = pass.toCharArray();
+    //BASE64Encoder myB64 = new BASE64Encoder();
 
-	char[] b64 = Base64Coder.encode(privKey.getEncoded());
+    File certificateFile = new File(fileName);
+    // System.out.println("certificateFile: " + certificateFile);
 
-	System.out.println("-----BEGIN PRIVATE KEY-----");
-	System.out.println(b64);
-	System.out.println("-----END PRIVATE KEY-----");
+    ks.load(new FileInputStream(certificateFile), passPhrase);
+    // System.out.println("ks: " + ks);
+    // for(String a : ks.aliases()) {
+    //   System.out.println("alias: " + a);
+    // }
 
-	}
+    KeyPair kp = getPrivateKey(ks, aliasName, passPhrase);
+    // System.out.println("kp: " + kp);
+    
+    PrivateKey privKey = kp.getPrivate();
+
+    char[] b64 = Base64Coder.encode(privKey.getEncoded());
+
+    System.out.println("-----BEGIN PRIVATE KEY-----");
+    for (String subSeq : splitArray(b64, 64)) {
+        System.out.println(subSeq.toCharArray());
+    }
+    System.out.println("-----END PRIVATE KEY-----");
+  }
 
 // From http://javaalmanac.com/egs/java.security/GetKeyFromKs.html
 
-   public KeyPair getPrivateKey(KeyStore keystore, String alias, char[] password) {
-        try {
-            // Get private key
-            Key key = keystore.getKey(alias, password);
-            if (key instanceof PrivateKey) {
-                // Get certificate of public key
-                Certificate cert = keystore.getCertificate(alias);
+  public KeyPair getPrivateKey(KeyStore keystore, String alias, char[] password) {
+    try {
+      // Get private key
+      Key key = keystore.getKey(alias, password);
+      if (key instanceof PrivateKey) {
+        // Get certificate of public key
+        Certificate cert = keystore.getCertificate(alias);
+
+        // Get public key
+        PublicKey publicKey = cert.getPublicKey();
     
-                // Get public key
-                PublicKey publicKey = cert.getPublicKey();
-    
-                // Return a key pair
-                return new KeyPair(publicKey, (PrivateKey)key);
-            }
-        } catch (UnrecoverableKeyException e) {
-        } catch (NoSuchAlgorithmException e) {
-        } catch (KeyStoreException e) {
-        }
-        return null;
+        // Return a key pair
+        return new KeyPair(publicKey, (PrivateKey)key);
+      }
+    } catch (UnrecoverableKeyException e) {
+    } catch (NoSuchAlgorithmException e) {
+    } catch (KeyStoreException e) {
     }
+    return null;
+  }
+
+  private Vector<String> splitArray(char[] chry, int subarrLen) {
+    Vector<String> result = new Vector<String>();
+    String input = new String(chry);
+    int i = 0;
+    while (i < chry.length) {
+      result.add(input.substring(i, Math.min(input.length(), i + subarrLen)));
+      i = i + subarrLen;
+    }
+    return result;
+  }
 
 }
 
